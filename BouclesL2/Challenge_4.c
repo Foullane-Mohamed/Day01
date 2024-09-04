86e150741f1783f3bc560fd8642cd77179023356
Foullane-Mohamed <mohamedfoullane4@gmail.com>
2024-09-04 16:34:01 +0200

    ex5 day2

--- BouclesL2/Challenge_4.c
+++ BouclesL2/Challenge_4.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 
 int main() { 
-  int nombre;
-    printf("saiser un nombre : ");
-    scanf("%d",&nombre);
- 
+ #include <stdio.h>
+
+int main(){
+    int size;
+    printf("Saiser  le nombre delements dun tableau : ");
+    scanf("%d",&size);
+
+    int tableau[size];
+
+    for(int i=0;i<size ;i++){
+        printf("Saiser  %i elements du tableau : " ,i+1);
+        scanf("%d",&tableau[i]);
+     
+       
+    }
+    for(int i=0;i<size ;i++){
+       
+     
+        printf("valure %d : %d \n", i+1, tableau[i]);
+    }
+   
+
+    return 0;
+}
     return 0; 
 } 
